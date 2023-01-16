c54c53ba2ae8450db9e69731b74a063aed5dfbd2
SubhamDey29 <118063699+SubhamDey29@users.noreply.github.com>
2023-01-16 09:23:03 +0000

    Update action.groovy

--- vars/action.groovy
+++ vars/action.groovy
@@ -1,10 +1,4 @@
-parameters{
-        choice(
-            name:'Action',
-            choices: ['Build','Destroy'],
-            description: 'What do you want to perform?'
-        )
-}
+
 def terraformAction()
     {
         if(params.Action=='Build'){
